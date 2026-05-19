c0b3acffa9e6cff355f99facad803fd51be6a9eb
Tochi Okoro <tookoro@uwaterloo.ca>
2026-05-18 21:20:40 -0400

    finally finished omg

--- src/robot/control/src/control_node.cpp
+++ src/robot/control/src/control_node.cpp
@@ -20,7 +20,12 @@ void ControlNode::odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg){
 }
 
 void ControlNode::controlLoop(){
-  if (!control_.current_path_ || !control_.robot_odom_ || control_.current_path_->poses.empty()){
+  if (!control_.current_path_ || !control_.robot_odom_ ){
+    return;
+  }
+
+  if (control_.current_path_->poses.empty()){
+    cmd_pub_->publish(geometry_msgs::msg::Twist());
     return;
   }
 
@@ -28,16 +33,15 @@ void ControlNode::controlLoop(){
   double dist_to_goal = computeDistance(control_.robot_odom_->pose.pose.position, goal_pos);
 
   if (dist_to_goal < goal_tolerance_) {
-    geometry_msgs::msg::Twist stop_cmd;
-    stop_cmd.linear.x = 0.0;
-    stop_cmd.angular.z = 0.0;
-    cmd_pub_->publish(stop_cmd);
+    cmd_pub_->publish(geometry_msgs::msg::Twist());
+    control_.current_path_ = nullptr;
     RCLCPP_INFO(this->get_logger(), "STOP");
     return; 
   }
 
   auto lookahead_point = findLookaheadPoint();
   if (!lookahead_point){
+    cmd_pub_->publish(geometry_msgs::msg::Twist());
     return;
   }
 
@@ -46,10 +50,6 @@ void ControlNode::controlLoop(){
 }
 
 std::optional<geometry_msgs::msg::PoseStamped> ControlNode::findLookaheadPoint(){
-  if (control_.current_path_->poses.empty()){
-    return std::nullopt;
-  }
-
   for (const auto &pose_stamped : control_.current_path_->poses){
     double distance = computeDistance(control_.robot_odom_->pose.pose.position, pose_stamped.pose.position);
     if (distance >= lookahead_distance_){
@@ -71,7 +71,7 @@ geometry_msgs::msg::Twist ControlNode::computeVelocity(const geometry_msgs::msg:
   double local_y = dx * std::sin(-robot_yaw) + dy * std::cos(-robot_yaw);
 
   double L_sq = (local_x * local_x) + (local_y * local_y);
-  if (L_sq < 0.001) {
+  if (L_sq == 0) {
     cmd_vel.linear.x = 0.0;
     cmd_vel.angular.z = 0.0;
     return cmd_vel;
@@ -80,7 +80,7 @@ geometry_msgs::msg::Twist ControlNode::computeVelocity(const geometry_msgs::msg:
   double curvature = (2.0 * local_y) / L_sq;
 
   cmd_vel.linear.x = linear_speed_;
-  cmd_vel.angular.z = cmd_vel.linear.x * curvature;
+  cmd_vel.angular.z = linear_speed_ * curvature;
 
   return cmd_vel;
 }
